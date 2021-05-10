f91f780ee7952109e088a58e672ade288a3497ed
sillykyle <sillykylew@yahoo.com>
2021-05-10 13:27:13 -0400

    Add route and curl script for index users Index users shows a list of all users that are signed up Also change name of curl-scripts/auth folder to curl-scripts/users

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -138,4 +138,18 @@ router.delete('/sign-out', requireToken, (req, res, next) => {
     .catch(next)
 })
 
+router.get('/users', requireToken, (req, res, next) => {
+  User.find()
+    .then(users => {
+      // `users` will be an array of Mongoose documents
+      // we want to convert each one to a POJO, so we use `.map` to
+      // apply `.toObject` to each one
+      return users.map(user => user.toObject())
+    })
+    // respond with status 200 and JSON of the examples
+    .then(users => res.status(200).json({ users: users }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
 module.exports = router
