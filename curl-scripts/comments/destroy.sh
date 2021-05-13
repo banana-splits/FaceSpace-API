434ef2c9664c14b4de6cefb575bb2b6c144654b4
Thomas Kim <thomaskim@Thomass-Mac-mini-2.local>
2021-05-13 15:50:05 -0400

    Destory Curl works

--- curl-scripts/comments/destroy.sh
+++ curl-scripts/comments/destroy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/comments"
+URL_PATH="/posts/${POST_ID}/comments/${COMMENT_ID}"
 
-curl "${API}${URL_PATH}/${COMMENT_ID}" \
+curl "${API}${URL_PATH}" \
   --include \
   --request DELETE \
   --header "Authorization: Bearer ${TOKEN}"
